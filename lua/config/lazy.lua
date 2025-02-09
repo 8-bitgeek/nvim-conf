2be0d42e8302046c6cf1dc940f85249d0eaf2a61
gldwolf <zengqiangzhao@outlook.com>
2025-02-09 09:50:38 +0800

    disable uptodate checker for lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,5 +21,5 @@ require("lazy").setup({
   spec = {{ import = "plugins" }},
   -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
