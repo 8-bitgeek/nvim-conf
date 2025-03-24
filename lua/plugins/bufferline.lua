fd92ec670dfd96db9d1752baff9b117869a49e60
gldwolf <zengqiangzhao@outlook.com>
2025-03-24 17:40:18 +0800

    add bufferline and update readme

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
+  keys = {
+      { "L", "<cmd>BufferLineCycleNext<cr>", desc = "Cycle Next Tab" },
+      { "H", "<cmd>BufferLineCyclePrev<cr>", desc = "Cycle Previous Tab" },
+  },
+  opts = {
+    options = {
+      numbers = "ordinal",
+      diagnostics = "nvim_lsp",
+      separator_style = "slant",
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          text_align = "center",
+        },
+      },
+      show_close_icon = true,
+      show_buffer_close_icons = true,
+    },
+  },
+}
+
