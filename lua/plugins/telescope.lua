8607dc3ccff2f1f71b24a63ccd5c3e9ccbd3baf4
gldwolf <zengqiangzhao@outlook.com>
2025-03-24 09:56:42 +0800

    update telescope keybinds for find file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },  -- 查找文件
+    { "<leader>o", "<cmd>Telescope find_files<cr>", desc = "Find Files" },  -- 查找文件
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },    -- 模糊搜索
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },  -- 查找缓冲区
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },   -- 查找帮助文档
