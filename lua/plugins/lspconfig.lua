e34640687a60d3395792eb94cc154e9273a73491
gldwolf <zengqiangzhao@outlook.com>
2025-03-26 15:14:52 +0800

    update lsp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,9 +5,17 @@ return {
     local lspconfig = require("lspconfig")
 
     lspconfig.lua_ls.setup {
-      on_attach = function(client, bufnr) 
-	print("Lua LSP Attached!")
-      end
+        on_attach = function(client, bufnr)
+            print("Lua LSP Attached!")
+        end
+    }
+
+    lspconfig.pyright.setup {
+        filetypes = {'python', 'py'},                       -- 指定触发 LSP 的文件类型(覆盖默认值)
+        root_dir = require("lspconfig.util").root_pattern(".git", "pyproject.toml"),    -- 项目根目录检测
+        on_attach = function(client, bufnr)
+            print("Python LSP Attached!")
+        end
     }
   end
 }
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { "williamboman/mason.nvim" },
   opts = {
     ensure_installed = {
-      "lua_ls",
+      "lua_ls"
     },
     automatic_installation = true,	-- 自动安装缺少的语言服务器
   }
