81e79a7c0914f39686474416fe665faa3f1a67c4
8-bitgeek <135525005+8-bitgeek@users.noreply.github.com>
2025-06-13 15:21:05 +0800

    Update tokyonight.lua: add comment color config

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,8 +2,16 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
   config = function()
-      vim.cmd([[colorscheme tokyonight-moon]])
+    require("tokyonight").setup({
+      style = "moon",
+      on_highlights = function(hl, colors)
+        hl.Comment = {
+          fg = "#FFFF00", -- 明亮黄色注释
+          italic = true,
+        }
+      end,
+    })
+    vim.cmd("colorscheme tokyonight")
   end,
 }
