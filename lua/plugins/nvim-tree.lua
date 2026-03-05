ca155e24447398845ac8ceb23a1b326997ac323c
gldwolf <zengqiangzhao@outlook.com>
2026-03-05 16:04:27 +0800

    feat: update nvim-tree.lua file color setting

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -13,4 +13,27 @@ return {
     keys = {
         { "<leader>g", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
     },
+    config = function(_, opts)
+        require("nvim-tree").setup(opts)
+
+        -- 设置更清晰的文件颜色
+        local function set_nvim_tree_hl()
+            vim.api.nvim_set_hl(0, "NvimTreeNormal", {
+                fg = "#ffffff",  -- 文件名颜色
+                bg = "#1e2030",  -- 背景（可不写）
+            })
+            vim.api.nvim_set_hl(0, "NvimTreeNormalNC", {
+                fg = "#ffffff",
+                bg = "#1e2030",
+            })
+        end
+
+        -- 立即执行一次
+        set_nvim_tree_hl()
+
+        -- 防止 colorscheme 覆盖
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            callback = set_nvim_tree_hl,
+        })
+    end,
 }
