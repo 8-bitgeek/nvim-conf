cbd8ffe09899bac37390f6c90990b961bfda8c73
gldwolf <zengqiangzhao@outlook.com>
2026-04-02 11:05:46 +0800

    feat: update line number color

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,18 +1,30 @@
 -- 主题
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "moon",
-      on_highlights = function(hl, colors)
-        hl.Comment = {
-          fg = "#FFFF00", -- 明亮黄色注释
-          italic = true,
-        }
-      end,
-    })
-    vim.cmd("colorscheme tokyonight")
-  end,
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("tokyonight").setup({
+            style = "moon",
+            on_highlights = function(hl, colors)
+                hl.Comment = {
+                    fg = "#FFFF00",     -- 明亮黄色注释
+                    italic = true,
+                }
+                hl.LineNrAbove = {
+                    fg = "#7aa2f7",     -- 上方相对行号(蓝色)
+                    bold = true,
+                }
+                hl.LineNrBelow = {
+                    fg = "#7aa2f7",
+                    bold = true,
+                }
+                hl.CursorLineNr = {
+                    fg = "#ff9e64",     -- 当前行号(橙色)
+                    bold = true,
+                }
+            end,
+        })
+        vim.cmd("colorscheme tokyonight")
+    end,
 }
